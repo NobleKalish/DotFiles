e4bbc079a0ffa3ee81414f7ef5c7276f7a0147c9
Noble Kalish <noblekalish1220@gmail.com>
2021-10-22 13:33:55 -0400

    added sim link to vimrc

--- setup.sh
+++ setup.sh
@@ -11,3 +11,8 @@ mkdir installs && cd installs
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt install ./installs/google-chrome-stable_current_amd64.deb
 cd ../ && rm -rf ./installs
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+ln -s ~/dotfiles/.vimrc ~/.vimrc
