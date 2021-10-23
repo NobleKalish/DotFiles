fb453e1f10e7d844a26dc2287875511b527cbe41
Noble Kalish <noblekalish1220@gmail.com>
2021-10-22 20:08:09 -0400

    kitty theme

--- setup.sh
+++ setup.sh
@@ -21,6 +21,8 @@ curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin || echo "Co
 ln -s ~/.local/kitty.app/bin/kitty ~/.local/bin/
 cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
 sed -i "s|Icon=kitty|Icon=/home/$USER/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty.desktop
+ln -s ~/dotfiles/kitty.conf ~/.config/kitty/kitty.conf
+ln -s ~/dotfiles/theme.conf ~/.config/kitty/theme.conf
 cd ../ && rm -rf ./installs
 
 echo "Setting git aliases"
